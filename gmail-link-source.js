7a2a66eeea3994596061a94a90f0f0f457c23fa0
Seth Battis <sbattis@groton.org>
2023-05-02 18:27:24 +0000

    new UI

--- gmail-link-source.js
+++ gmail-link-source.js
@@ -1,4 +1,4 @@
-const emailElt = document.querySelector('.gb_zc :nth-child(3)');
+const emailElt = document.querySelector('.gb_Ab').nextElementSibling;
 const email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
 navigator.clipboard.writeText(
   window.location.href.replace(/(\/u\/0)\/.*\/(.+$)/, `/u/${email}/#all/$2`)
