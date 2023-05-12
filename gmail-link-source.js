2ead0f5dbb1df4de7f2c2f3e735a35800b1063f6
Seth Battis <sbattis@groton.org>
2023-05-12 17:21:18 +0000

    Simpler #1

--- gmail-link-source.js
+++ gmail-link-source.js
@@ -1,10 +1,5 @@
-const emailElt = document.querySelector('.gb_Ab').nextElementSibling;
-const email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
+emailElt = document.querySelector('.gb_Ab').nextElementSibling;
+email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
 navigator.clipboard.writeText(
   window.location.href.replace(/(\/u\/0)\/.*\/(.+$)/, `/u/${email}/#all/$2`)
 );
-document
-  .querySelector(
-    'script[src="https://groton-school.github.io/gmail-link-bookmarklet/gmail-link-source.js"])'
-  )
-  .remove();
