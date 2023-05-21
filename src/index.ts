35ca397dee4731b9d91965f256165f4845692d4d
Seth Battis <sbattis@groton.org>
2023-05-21 16:40:39 -0400

    get subject

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@
   );
   const id = `notification-${crypto.randomUUID()}`;
   const message = document.createElement('div');
+  const subject = (document.querySelector('#:1s') as HTMLElement).innerText;
   message.id = id;
   message.setAttribute(
     'style',
-    'position: absolute; z-index: 100; right: 1em; top: 1em; background: lightgoldenrod; border: solid 1px goldenrod; border-radius: 0.5em; padding: 0.5em;'
+    'position: absolute; z-index: 100; right: 1em; top: 1em; background: lightgoldenrodyellow; border: solid 1px goldenrod; border-radius: 0.5em; padding: 0.5em;'
   );
-  message.innerText = 'Copied message link to clipboard';
+  message.innerText = `Copied message link to "${subject}" to  clipboard`;
   document.body.append(message);
   setTimeout(() => document.body.querySelector(`#${id}`)?.remove(), 1000);
 })();
