03b2fecbd47d2d09e5572bce7e09030fa5b9d7ff
Seth Battis <sbattis@groton.org>
2023-05-20 20:45:37 -0400

    duh, wrap it

--- gmail-link-source.js
+++ gmail-link-source.js
@@ -1,5 +1,7 @@
-emailElt = document.querySelector('.gb_Ab').nextElementSibling;
-email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
-navigator.clipboard.writeText(
-  window.location.href.replace(/(\/u\/0)\/.*\/(.+$)/, `/u/${email}/#all/$2`)
-);
+(() => {
+  const emailElt = document.querySelector('.gb_Ab').nextElementSibling;
+  const email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
+  navigator.clipboard.writeText(
+    window.location.href.replace(/(\/u\/0)\/.*\/(.+$)/, `/u/${email}/#all/$2`)
+  );
+})();
