d3f15722cd0ebb77a4483e8e8eea95559370e940
Seth Battis <sbattis@groton.org>
2023-07-05 07:13:12 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import './styles.scss';
 
 (() => {
-  const emailElt = document.querySelector('.gb_Ab')
-    ?.nextElementSibling as HTMLElement;
+  const emailElt = document.querySelector('.gb_Ec div:not(.gb_Eb):last-child') as HTMLElement;
   const email = emailElt && emailElt.innerText ? emailElt.innerText : '0';
   navigator.clipboard.writeText(
     window.location.href.replace(/(\/u\/0)\/.*\/(.+$)/, `/u/${email}/#all/$2`)
